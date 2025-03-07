a28e95b7700387e1c4bac9c26a377f289c530dd9
Nicolas Rios <riosc.nicolas@gmail.com>
2025-03-07 13:05:03 -0300

    Disposables WHOIS to process

--- check_domain_whois.py
+++ check_domain_whois.py
@@ -6,7 +6,7 @@ import requests
 from datetime import timezone
 
 # Load CSV file
-df = pd.read_csv("df7.csv")  # Replace with your actual file
+df = pd.read_csv("disposables/disposables.csv")  # Replace with your actual file
 
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
@@ -70,7 +70,7 @@ def fetch_whois_data(domain, retries=5, delay_range=(2, 5)):
 
 # Process domains
 data_list = []
-print("Analyzing df7.csv")
+print("Analyzing disposables.csv")
 
 for index, domain in enumerate(df["domain"], start=1):
     whois_data = fetch_whois_data(domain)
@@ -88,5 +88,5 @@ whois_df = pd.DataFrame(data_list)
 df = df.merge(whois_df, on="domain", how="left")
 
 # Save to CSV
-df.to_csv("df7_enriched.csv", index=False)
-print("WHOIS data collection completed and saved to df7_enriched.csv")
+df.to_csv("disposables/disposables_enriched.csv", index=False)
+print("WHOIS data collection completed and saved to disposables_enriched.csv")
