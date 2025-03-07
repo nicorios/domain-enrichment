834b3d54f05610125d66496d51347665f3d382bc
Nicolas Rios <riosc.nicolas@gmail.com>
2025-03-07 09:02:28 -0300

    DF7 WHOIS to process

--- check_domain_whois.py
+++ check_domain_whois.py
@@ -5,7 +5,7 @@ import random
 import requests
 
 # Load CSV file
-df = pd.read_csv("whois/df3_2.csv")  # Replace with your actual file
+df = pd.read_csv("whois/df7.csv")  # Replace with your actual file
 
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
@@ -60,7 +60,7 @@ def fetch_whois_data(domain, retries=5, delay_range=(2, 5)):
 
 # Process domains
 data_list = []
-print("Analyzing df3_2.csv")
+print("Analyzing df7.csv")
 
 for index, domain in enumerate(df["domain"], start=1):
     whois_data = fetch_whois_data(domain)
@@ -78,5 +78,5 @@ whois_df = pd.DataFrame(data_list)
 df = df.merge(whois_df, on="domain", how="left")
 
 # Save to CSV
-df.to_csv("whois/df3_enriched.csv", index=False)
+df.to_csv("whois/df7_enriched.csv", index=False)
 print("WHOIS data collection completed and saved to df5_enriched.csv")
