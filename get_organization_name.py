c4657d0aac8a5c0eb5f469d2095028beb927990e
Nicolas Rios <riosc.nicolas@gmail.com>
2025-03-13 13:04:51 -0500

    DF7 to process

--- get_organization_name.py
+++ get_organization_name.py
@@ -155,13 +155,13 @@ def scrape_website_info(domain):
 
 
 # Load CSV file with domains
-df = pd.read_csv("df6.csv")  # Assuming a CSV with a "domain" column
+df = pd.read_csv("df7.csv")  # Assuming a CSV with a "domain" column
 
 # Add the new column for best match
 df["best_site_name"] = None
 df["scraping_status_code"] = None
 
-print("Analyzing df6.csv")
+print("Analyzing df7.csv")
 for i, (index, row) in enumerate(df.iterrows(), start=1):  # Ensure proper iteration
     domain = row["domain"]  # Extract domain correctly
     best_name, status_code = scrape_website_info(domain)
@@ -175,6 +175,6 @@ for i, (index, row) in enumerate(df.iterrows(), start=1):  # Ensure proper itera
 
 
 # Save updated dataframe with the appended column
-df.to_csv("df6-ready.csv", index=False)
+df.to_csv("df7-ready.csv", index=False)
 
-print("✅ Scraping complete. Results saved to df6-ready.csv.")
+print("✅ Scraping complete. Results saved to df7-ready.csv.")
