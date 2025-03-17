c4d4b1328a7357f2f0a080c332acedba23bc5572
Nicolas Rios <riosc.nicolas@gmail.com>
2025-03-17 13:18:29 -0500

    missing7 to process

--- get_org_name_abstract.py
+++ get_org_name_abstract.py
@@ -134,11 +134,11 @@ def fetch_website_info(domain):
     }
     return determine_best_name(domain, names), response.status_code
 
-df = pd.read_csv("missing_6.csv")
+df = pd.read_csv("missing_7.csv")
 df["best_site_name"] = None
 df["scraping_status_code"] = None
 
-print("Analyzing missing_6.csv")
+print("Analyzing missing_7.csv")
 for i, (index, row) in enumerate(df.iterrows(), start=1):
     domain = row["domain"]
     best_name, status_code = fetch_website_info(domain)
@@ -147,5 +147,5 @@ for i, (index, row) in enumerate(df.iterrows(), start=1):
     if i % 500 == 0 or i == 10:
         print(f"📢 {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Processed {i} rows")
 
-df.to_csv("missing_6-ready.csv", index=False)
-print("✅ Scraping complete. Results saved to missing_6-ready.csv.")
+df.to_csv("missing_7-ready.csv", index=False)
+print("✅ Scraping complete. Results saved to missing_7-ready.csv.")
