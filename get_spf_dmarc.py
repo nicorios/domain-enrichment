f2f00acf8c6f6776f2f342fed1fc9095ec7dda96
Nicolas Rios <riosc.nicolas@gmail.com>
2025-03-20 07:30:28 -0500

    df3 to process

--- get_spf_dmarc.py
+++ get_spf_dmarc.py
@@ -90,10 +90,10 @@ def is_live_site(domain):
     return False
 
 
-df = pd.read_csv("df2.csv")
+df = pd.read_csv("df3.csv")
 
 # Process each row and print progress every 500 rows
-print("Analyzing df2.csv")
+print("Analyzing df3.csv")
 for i, domain in enumerate(df['domain']):
     df.at[i, 'is_spf_strict'] = get_spf_strict(domain)
     df.at[i, 'is_dmarc_enforced'] = get_dmarc_policy(domain)
@@ -104,5 +104,5 @@ for i, domain in enumerate(df['domain']):
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         print(f"📢 {current_time} - Processed {i+1} rows")
 
-df.to_csv("df2-ready.csv", index = False)
-print("✅ Results saved to df2-ready.csv.")
+df.to_csv("df3-ready.csv", index = False)
+print("✅ Results saved to df3-ready.csv.")
