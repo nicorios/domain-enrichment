976c5a7e64dc68eb6abd533e8d17ca2e967e36e6
Nicolas Rios <riosc.nicolas@gmail.com>
2025-04-04 09:13:23 -0500

    adjusted to get domains from 7 days ago

--- domain_enrichment/domain_fetcher.py
+++ domain_enrichment/domain_fetcher.py
@@ -5,7 +5,7 @@ import pandas as pd
 def get_new_domains():
     url = "https://raw.githubusercontent.com/7c/fakefilter/refs/heads/main/json/data_version2.json"
     now = int(time.time())
-    one_day_ago = now - 86400
+    seven_days_ago = now - 604800
 
     response = requests.get(url)
     data = response.json()
@@ -13,7 +13,7 @@ def get_new_domains():
 
     for domain, info in data.get("domains", {}).items():
         for host, details in info.get("hosts", {}).items():
-            if details.get("firstseen", 0) >= one_day_ago:
+            if details.get("firstseen", 0) >= seven_days_ago:
                 rows.append({"domain": domain, "firstseen": details.get("firstseen")})
                 break
 
